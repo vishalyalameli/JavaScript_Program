65fc3786784220a08251d614f0d68294c8ebd00c
vishal <vishal.yalameli@whitecode.in>
2024-10-20 15:18:05 +0530

    FindMinNumberArray

--- findmaxNumberArray.js
+++ findmaxNumberArray.js
@@ -1,17 +1,38 @@
 //Write a JavaScript program to find the maximum number in an array
-function FindmaxNumberArray (arr){
-    let max = [0]
+// function FindmaxNumberArray (arr){
+//     let max = [0]
 
-    for (let i =1; i<arr.length; i++){
-        if(arr [i] > max){
-            max = arr[i];
-        }
-    }
-    return max
-}
+//     for (let i =1; i<arr.length; i++){
+//         if(arr [i] > max){
+//             max = arr[i];
+//         }
+//     }
+//     return max
+// }
 
-let number = [10,20,30,40,50];
-let maxNumber = FindmaxNumberArray(number);
-console.log("The maximum Number is :", maxNumber);
+// let number = [10,20,30,40,50];
+// let maxNumber = FindmaxNumberArray(number);
+// console.log("The maximum Number is :", maxNumber);
 
 // The maximum Number is : 50
+
+ //Write a JavaScript program to find the minimum number in an array
+
+function FindMinNumberArray (arr) {
+
+    let min = arr[0]
+
+    for(let i = 1; i< arr.length; i++){
+        if(arr[i]< min){
+            min = arr[i];
+        }
+
+    }
+    return min; 
+
+}
+
+let  Number = [10,20,30,40,50];
+let MinNumber = FindMinNumberArray(Number);
+
+console.log("The Minimum Number is:", MinNumber);
