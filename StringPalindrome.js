08d77046ced4f54760d81afa156418ad425152c4
vishal <vishal.yalameli@whitecode.in>
2024-10-21 21:48:32 +0530

    isPalindrome

--- StringPalindrome.js
+++ StringPalindrome.js
@@ -1,19 +1,31 @@
 //Write a JavaScript function to check if a given string is a palindrome (reads the same forwards and backwards). 
 
+// function isPalindrome(str){
+
+//     let lowerStr = str.toLowerCase();
+
+//     let reverserd = '';
+//     for(let i = lowerStr.length-1 ; i>= 0; i--){
+//         reverserd +=lowerStr[i];
+//     }
+//     if(lowerStr === reverserd){
+//         return true; 
+//     }else{
+//         return false
+//     }
+
+// }
+// console.log(isPalindrome('racecar'));
+// console.log(isPalindrome('hello'));
+
+
 function isPalindrome(str){
 
-    let lowerStr = str.toLowerCase();
+    const reverserd =str. replace(/[^a-z0-9]/gi, '').toLowerCase();
 
-    let reverserd = '';
-    for(let i = lowerStr.length-1 ; i>= 0; i--){
-        reverserd +=lowerStr[i];
-    }
-    if(lowerStr === reverserd){
-        return true; 
-    }else{
-        return false
-    }
+    const reverse = reverserd.split('').reverse().join('');
 
+     return reverse === reverserd;
 }
-console.log(isPalindrome('racecar'));
-console.log(isPalindrome('hello'));
+console.log(isPalindrome('racecar')); //true
+console.log(isPalindrome('hello')); //false
