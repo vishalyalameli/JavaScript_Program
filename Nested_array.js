8b85aeb1577407f64616e5a5e34603959ce8d074
vishal <vishal.yalameli@whitecode.in>
2024-10-25 00:28:58 +0530

    NesteArray

--- Nested_array.js
+++ Nested_array.js
@@ -1,16 +1,38 @@
 // Write a JavaScript program to find the largest element in a nested array. 
 
-function isLargestArray(arr){
+// function isLargestArray(arr){
+//     let largest = -Infinity;
+
+//     function findLargest(arr){
+//         for(let element of arr){
+//             if(Array.isArray(element)){
+//                 findLargest(element);
+//             }else{
+//                 if(element > largest){
+//                     largest = element;
+//                 }
+//             }
+//         }
+//     }
+//     findLargest(arr);
+//     return largest;
+
+// }
+// const number = [1,[2,[3,[8,[45]]]]]
+// console.log(isLargestArray(number));
+
+function isLargest(arr){
     let largest = -Infinity;
 
-    function findLargest(arr){
+    function findLargest (arr){
         for(let element of arr){
             if(Array.isArray(element)){
-                findLargest(element);
+                findLargest(element)
             }else{
                 if(element > largest){
-                    largest = element;
+                    largest = element
                 }
+
             }
         }
     }
@@ -18,6 +40,8 @@ function isLargestArray(arr){
     return largest;
 
 }
-const number = [1,[2,[3,[8,[45]]]]]
-console.log(isLargestArray(number));
+const number = [1,2,[,3,[,8,[,6,5,[,6,5,6[5]]]]]]
+console.log(isLargest(number));
+
+
 
