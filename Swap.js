6e090fe421fe11b67da36512c30a5839183e034f
vishal <vishal.yalameli@whitecode.in>
2024-11-23 00:28:38 +0530

    Swap pro

--- Swap.js
+++ Swap.js
@@ -1,5 +1,14 @@
-let = 10;
+
+//swap the  two variable without using 3rd variable
+let a = 5;
 let b = 20;
 
 [a, b] = [b ,a]
 console.log(a,b);
+
+// a= a + b ;
+// b = a- b ;
+// a =  a -b ;
+
+// console.log("a:", a, "b:", b );
+
