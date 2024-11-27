b8334961a0d8595e8cdd33ff79a0d2b6e7413c5a
vishal <vishal.yalameli@whitecode.in>
2024-11-27 20:14:08 +0530

    Reverse pro

---
+++ Array.js
@@ -0,0 +1,23 @@
+
+// Constructor Array
+let fruit = new Array("mango","Apple","Pizza");
+
+console.log(fruit);
+
+// using arry literal
+
+let fruitt =["a","b","c"];
+console.log(fruitt);
+
+// eg empty array
+
+let fruits = ["a" ,"b","c","d"]
+console.log(fruits[2]); // 2 c
+
+// changes
+
+let fruitss = ["mango", "banna","apple","orange"]
+fruitss[4] ="Vishal";
+console.log(fruitss);
+
+// [ 'mango', 'banna', 'apple', 'orange', 'Vishal' ]
