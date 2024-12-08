8ada8c6179ae4ee81052e36f5d84981dfe129139
vishal <vishal.yalameli@whitecode.in>
2024-12-08 23:31:15 +0530

    Pattern program for the right angle

---
+++ Pattern_Program/Right_Angled_Triangle.js
@@ -0,0 +1,11 @@
+function rightAngle(row){
+    for(let i = 1; i<=row; i++){
+        let row = ' ';
+        for(let j =1; j <= i; j++){
+            row +=  '*';
+        }
+        console.log(row);
+    }
+
+}
+rightAngle(5);
