c43458b7f29c77e79f7d3445da2d81889d2737e7
vishal <yalamelivishal123@gmail.com>
2024-12-11 23:56:04 +0530

    Diamond code

---
+++ Pattern_Program/Diamond.js
@@ -0,0 +1,12 @@
+function diamond(rows) {
+    for (let i = 1; i <= rows; i++) {
+      let row = ' '.repeat(rows - i) + '*'.repeat(2 * i - 1);
+      console.log(row);
+    }
+    for (let i = rows - 1; i >= 1; i--) {
+      let row = ' '.repeat(rows - i) + '*'.repeat(2 * i - 1);
+      console.log(row);
+    }
+  }
+  diamond(5);
+  
